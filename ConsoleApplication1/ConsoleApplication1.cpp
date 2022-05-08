e7ba9ec227beae53f9be6391e13b9dc66d6fbf8f
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2022-05-08 21:01:05 +0930

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int foo;
+   //auto foo2; we need initialization for auto and that is why it causes the errors
+    int bar = 10;
+    //sum = 0;
+    float price = 10.5, cost = 11.1;
+    auto value = 5.6;
+    auto value = 9.5f;
+    auto variable = value;
+    int c = 0, d = { 1 }, e(0);
+    //int 1a = 3;
+}
+
